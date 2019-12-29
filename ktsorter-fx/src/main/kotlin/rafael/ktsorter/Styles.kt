d887aed11016fea14f9d04e2a7ea7eab2fbaa667
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2019-12-29 14:56:04 -0300

    screen layout

--- ktsorter-fx/src/main/kotlin/rafael/ktsorter/Styles.kt
+++ ktsorter-fx/src/main/kotlin/rafael/ktsorter/Styles.kt
@@ -1,23 +1,55 @@
 package rafael.ktsorter
 
+import javafx.geometry.HPos
+import javafx.geometry.Pos
 import javafx.scene.paint.Color
-import javafx.scene.paint.CycleMethod
-import javafx.scene.paint.LinearGradient
-import javafx.scene.paint.Stop
 import javafx.scene.text.FontWeight
 import tornadofx.*
 
 class Styles : Stylesheet() {
     companion object {
-        val welcomeScreen by cssclass()
+        val mainScreen by cssclass()
+        val pad  by cssclass()
         val content by cssclass()
         val heading by cssclass()
+        val controlsFields by cssclass()
+        val labels  by cssclass()
+        val buttons  by cssclass()
+        val texts by cssclass()
+        val canvases by cssclass()
     }
 
     init {
-        welcomeScreen {
+        mainScreen {
+            prefHeight = 600.px
+            prefWidth = 1280.px
             padding = box(10.px)
-            backgroundColor += LinearGradient(0.0, 0.0, 0.0, 1.0, true, CycleMethod.NO_CYCLE, Stop(0.0, c("#028aff")), Stop(1.0, c("#003780")))
+//            backgroundColor += LinearGradient(0.0, 0.0, 0.0, 1.0, true, CycleMethod.NO_CYCLE, Stop(0.0, c("#028aff")), Stop(1.0, c("#003780")))
+            pad {
+                box(10.px)
+            }
+            controlsFields {
+                padding = box(10.px)
+                startMargin = 10.px
+                endMargin = 10.px
+            }
+            labels {
+                fontWeight = FontWeight.BOLD
+                alignment = Pos.CENTER_RIGHT
+            }
+            buttons {
+                prefWidth = 200.px
+                endMargin = 10.px
+                hAlignment = HPos.CENTER
+            }
+            texts {
+                alignment = Pos.CENTER_RIGHT
+            }
+            canvases {
+                backgroundColor= multi(Color.WHITE)
+            }
+
+
             heading {
                 fontSize = 3.em
                 textFill = Color.WHITE
--- ktsorter-fx/src/main/kotlin/rafael/ktsorter/views/MainView.kt
+++ ktsorter-fx/src/main/kotlin/rafael/ktsorter/views/MainView.kt
@@ -1,25 +1,166 @@
 package rafael.ktsorter.views
 
+import javafx.collections.FXCollections
+import javafx.geometry.Pos
+import javafx.scene.canvas.Canvas
+import javafx.scene.control.Button
+import javafx.scene.control.CheckBox
+import javafx.scene.control.ComboBox
+import javafx.scene.control.TextField
+import javafx.scene.layout.Pane
 import rafael.ktsorter.Styles
-import javafx.scene.control.Alert.AlertType.INFORMATION
 import tornadofx.*
 
-class MainView : View("Hello TornadoFX") {
+class MainView : View("KTSorter") {
+
+    private lateinit var pnlControls: Pane
+    private lateinit var cmbQuantity: ComboBox<Int>
+    private lateinit var cmbSequenceType: ComboBox<String>
+    private lateinit var cmbExihibitionType: ComboBox<String>
+    private lateinit var cmbSortingType: ComboBox<String>
+    private lateinit var cmbIntervalCycles: ComboBox<Int>
+    private lateinit var chbSound: CheckBox
+    private lateinit var btnGenerateNumbers: Button
+    private lateinit var btnSort: Button
+    private lateinit var btnReset: Button
+    private lateinit var txfComparsions: TextField
+    private lateinit var txfSwaps: TextField
+    private lateinit var txfTime: TextField
+    private lateinit var canvas: Canvas
+
+    init {
+        super.primaryStage.isResizable = false
+    }
+
     override val root = borderpane {
-        addClass(Styles.welcomeScreen)
-        top {
-            stackpane {
-                label(title).addClass(Styles.heading)
+        addClass(Styles.mainScreen)
+        left {
+            pnlControls = form {
+                vbox(spacing = 10, alignment = Pos.CENTER) {
+                    fieldset {
+                        field("Quantity") {
+                            addClass(Styles.controlsFields)
+                            label.addClass(Styles.labels)
+                            cmbQuantity = combobox {
+                                items = FXCollections.observableArrayList(5, 10, 20, 50, 100, 200, 500, 1000)
+                                value = 50
+                            }
+                            label.labelFor = cmbQuantity
+                        }
+                        field("Sequence Type") {
+                            addClass(Styles.controlsFields)
+                            label.addClass(Styles.labels)
+                            cmbSequenceType = combobox {
+                                items = FXCollections.observableArrayList(
+                                    "Random",
+                                    "Unique",
+                                    "Crescent",
+                                    "Decrescent",
+                                    "4 Values",
+                                    "Semi sorted"
+                                )
+                                value = items[0]
+                            }
+                            label.labelFor = cmbSequenceType
+                        }
+                        field("Exihibition Type") {
+                            addClass(Styles.controlsFields)
+                            label.addClass(Styles.labels)
+                            cmbExihibitionType = combobox {
+                                items = FXCollections.observableArrayList("Spectre", "Points")
+                            }
+                            label.labelFor = cmbExihibitionType
+                        }
+                        field("Sorting Type") {
+                            addClass(Styles.controlsFields)
+                            label.addClass(Styles.labels)
+                            cmbSortingType = combobox {
+                                items = FXCollections.observableArrayList(
+                                    "Bubble",
+                                    "Cocktail",
+                                    "Pancake",
+                                    "Selection",
+                                    "Insertion",
+                                    "Circle"
+                                )
+                            }
+                            label.labelFor = cmbSortingType
+                        }
+                        field("Interval Cycle (ms)") {
+                            addClass(Styles.controlsFields)
+                            label.addClass(Styles.labels)
+                            cmbIntervalCycles = combobox {
+                                items = FXCollections.observableArrayList(0, 1, 2, 5, 10, 20, 50)
+                                value = 10
+                            }
+                            label.labelFor = cmbIntervalCycles
+                        }
+                        field("Ativar som") {
+                            addClass(Styles.controlsFields)
+                            label.addClass(Styles.labels)
+                            chbSound = checkbox {
+                                isSelected = true
+                                isDisable = true
+                            }
+                        }
+                    }
+                    separator {
+                        addClass(Styles.pad)
+                    }
+                    btnGenerateNumbers = button {
+                        text = "Generate Number"
+                        addClass(Styles.buttons)
+                    }
+                    btnSort = button {
+                        text = "Sort"
+                        addClass(Styles.buttons)
+                    }
+                    btnReset = button {
+                        text = "Reset"
+                        addClass(Styles.buttons)
+                    }
+                    separator {
+                        addClass(Styles.pad)
+                    }
+                    fieldset {
+                        field("Comparsions") {
+                            addClass(Styles.controlsFields)
+                            label.addClass(Styles.labels)
+                            txfComparsions = textfield {
+                                text = "9999"
+                                isEditable = false
+                                addClass(Styles.texts)
+                            }
+                        }
+                        field("Swaps") {
+                            addClass(Styles.controlsFields)
+                            label.addClass(Styles.labels)
+                            txfSwaps = textfield {
+                                text = "9999"
+                                isEditable = false
+                                addClass(Styles.texts)
+                            }
+                        }
+                        field("Time") {
+                            addClass(Styles.controlsFields)
+                            label.addClass(Styles.labels)
+                            txfTime = textfield {
+                                text = "9999"
+                                isEditable = false
+                                addClass(Styles.texts)
+                            }
+                        }
+                    }
+                }
             }
         }
         center {
-            stackpane {
-                addClass(Styles.content)
-                button("Click me") {
-                    setOnAction {
-                        alert(INFORMATION, "Well done!", "You clicked me!")
-                    }
+            group {
+                addClass(Styles.canvases)
+                canvas = canvas {
                 }
+                canvas.height = height
+                canvas.width = width
             }
         }
     }
