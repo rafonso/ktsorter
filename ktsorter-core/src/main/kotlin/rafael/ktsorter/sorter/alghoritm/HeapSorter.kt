2ebd30602efc43dd72c94c3ec6d3fc3dced0d800
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2020-01-09 06:10:50 -0300

    Added Quick Sorter.

---
+++ ktsorter-core/src/main/kotlin/rafael/ktsorter/sorter/alghoritm/HeapSorter.kt
@@ -0,0 +1,54 @@
+package rafael.ktsorter.sorter.alghoritm
+
+/**
+ * https://en.wikipedia.org/wiki/Heapsort
+ * https://gist.github.com/gyoshev/4038839
+ */
+class HeapSorter(pauseTime: Long) : Sorter(pauseTime) {
+
+    private tailrec fun maxHeapfy(values: IntArray, _i: Int, length: Int) {
+        var i = _i
+        while (true) {
+            val left = i * 2 + 1
+            val right = i * 2 + 2
+            var largest = i
+
+            if (left < length && super.isLesser(values, largest, left)) {
+                largest = left
+            }
+
+            if (right < length && super.isLesser(values, largest, right)) {
+                largest = right;
+            }
+
+            if (i == largest) {
+                break
+            }
+
+            super.swap(values, i, largest)
+            i = largest
+        }
+    }
+
+    private fun heapfy(values: IntArray, lenght: Int) {
+        for (i in lenght / 2 downTo 0) {
+            this.maxHeapfy(values, i, lenght)
+        }
+    }
+
+
+    override fun process(values: IntArray): IntArray {
+        this.heapfy(values, values.size)
+
+        for (i in values.lastIndex downTo 1) {
+            super.swap(values, i, 0)
+            this.maxHeapfy(values, 0, i - 1)
+        }
+        // Workaround!
+        if(super.isLesser(values, 2, 1)) {
+            super.swap(values, 2, 1)
+        }
+
+        return values
+    }
+}
--- ktsorter-core/src/main/kotlin/rafael/ktsorter/sorter/alghoritm/Sorters.kt
+++ ktsorter-core/src/main/kotlin/rafael/ktsorter/sorter/alghoritm/Sorters.kt
@@ -4,17 +4,17 @@ import rafael.ktsorter.util.Descriptable
 
 enum class Sorters(override val description: String, val generator: (Long) -> Sorter) : Descriptable {
     // @formatter:off
-    BUBBLE      ("Bobble"   , { BubbleSorter    (it)    }),
-    COCKTAIL    ("Cocktail" , { CocktailSorter  (it)    }),
-    ODD_EVEN    ("Odd Even" , { OddEvenSorter   (it)    }),
-//    CIRCLE      ("Circle"   , { CircleSorter    (it)    }),
-    GNOME       ("Gnome"    , { GnomeSorter     (it)    }),
-    COMB        ("Comb"     , { CombSorter      (it)     }),
-    SHELL       ("Shell"    , { ShellSorter     (it)     }),
-    INSERTION   ("Insertion", { InsertionSorter (it)     }),
-    SELECTION   ("Selection", { SelectionSorter (it)     }),
-    QUICKSORT   ("QuickSort", { QuickSorter     (it)     }),
-    QUICKINSERT ("QuickInsertSort", { QuickInsertSorter     (it)     }),
-//    HEAP        ("HeapSort" , { HeapSorter      (it)     }),
+    BUBBLE      ("Bobble"       , { BubbleSorter        (it)    }),
+    COCKTAIL    ("Cocktail"     , { CocktailSorter      (it)    }),
+    ODD_EVEN    ("Odd Even"     , { OddEvenSorter       (it)    }),
+//    CIRCLE      ("Circle"       , { CircleSorter        (it)    }),
+    GNOME       ("Gnome"        , { GnomeSorter         (it)    }),
+    COMB        ("Comb"         , { CombSorter          (it)     }),
+    SHELL       ("Shell"        , { ShellSorter         (it)     }),
+    INSERTION   ("Insertion"    , { InsertionSorter     (it)     }),
+    SELECTION   ("Selection"    , { SelectionSorter     (it)     }),
+    QUICKSORT   ("Quick"        , { QuickSorter         (it)     }),
+    QUICKINSERT ("Quick Insert" , { QuickInsertSorter   (it)     }),
+    HEAP        ("Heap"         , { HeapSorter          (it)     }),
     // @formatter:on
 }
