a1cfb5c566b4142a58dc8cbd80d22852627744c5
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2020-01-10 20:30:47 -0300

    Limits using powers of 2

--- ktsorter-fx/src/main/kotlin/rafael/ktsorter/views/plot/Limits.kt
+++ ktsorter-fx/src/main/kotlin/rafael/ktsorter/views/plot/Limits.kt
@@ -3,14 +3,14 @@ package rafael.ktsorter.views.plot
 import rafael.ktsorter.util.Descriptable
 
 val limitsValues = listOf(
-    Limits(   5, 2.0),
-    Limits(  10, 2.0),
-    Limits(  20, 2.0),
-    Limits(  50, 2.0),
-    Limits( 100, 2.0),
-    Limits( 200, 1.0),
-    Limits( 500, 1.0),
-    Limits(1000, 0.5)
+    Limits(2 shl 2, 2.0),
+    Limits(2 shl 3, 2.0),
+    Limits(2 shl 4, 2.0),
+    Limits(2 shl 5, 2.0),
+    Limits(2 shl 6, 2.0),
+    Limits(2 shl 7, 1.0),
+    Limits(2 shl 8, 1.0),
+    Limits(2 shl 9, 0.5)
 )
 
 data class Limits(val quantity: Int, val radius: Double) : Descriptable {
