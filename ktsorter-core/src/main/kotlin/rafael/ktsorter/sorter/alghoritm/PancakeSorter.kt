cc2d67ff310368bcb81a6673e23cd3398c1467ff
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2020-01-12 08:05:39 -0300

    Added Pancake Sorter

---
+++ ktsorter-core/src/main/kotlin/rafael/ktsorter/sorter/alghoritm/PancakeSorter.kt
@@ -0,0 +1,51 @@
+package rafael.ktsorter.sorter.alghoritm
+
+/**
+ * https://en.wikipedia.org/wiki/Pancake_sorting
+ * https://rosettacode.org/wiki/Sorting_algorithms/Pancake_sort#JavaScript
+ */
+class PancakeSorter(pauseTime: Long) : Sorter(pauseTime, INFO.type) {
+
+    companion object {
+        val INFO = SortInfo("Pancake", SortType.OTHER, AveragePerformance.N)
+    }
+
+    private tailrec fun findMaxIdx(values: IntArray, n: Int, j: Int = 1, maxIdx: Int = 0): Int {
+        if (j > n) {
+            return maxIdx
+        }
+
+        val nextMaxIdx = if (!super.isLesser(values, j, maxIdx)) j else maxIdx
+
+        return findMaxIdx(values, n, j + 1, nextMaxIdx)
+    }
+
+    private tailrec fun flip(values: IntArray, i: Int, j: Int) {
+        if (i >= j) {
+            return
+        }
+
+        super.swap(values, i, j)
+
+        flip(values, i + 1, j - 1)
+    }
+
+    private tailrec fun sort(values: IntArray, i: Int) {
+        if (i == 0) {
+            return
+        }
+
+        val maxIdx = findMaxIdx(values, i)
+        if (maxIdx < i) {
+            flip(values, maxIdx, i)
+        }
+
+        sort(values, i - 1)
+    }
+
+    override fun process(values: IntArray): IntArray {
+        sort(values, values.lastIndex)
+
+        return values
+    }
+}
--- ktsorter-core/src/main/kotlin/rafael/ktsorter/sorter/alghoritm/Sorters.kt
+++ ktsorter-core/src/main/kotlin/rafael/ktsorter/sorter/alghoritm/Sorters.kt
@@ -19,6 +19,7 @@ enum class Sorters(val info: SortInfo, val generator: (Long) -> Sorter) : Descri
     QUICKSORT   (QuickSorter       .INFO, { QuickSorter        (it)}),
     QUICKINSERT (QuickInsertSorter .INFO, { QuickInsertSorter  (it)}),
     HEAP        (HeapSorter        .INFO, { HeapSorter         (it)}),
+    PANCAKE     (PancakeSorter     .INFO, { PancakeSorter      (it)}),
     // @formatter:on
     ;
 
